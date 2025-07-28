79c33964a6dca6e8c9433afcd8828c3259bdb551
Lovro <lovro.zskrlj@gmail.com>
2025-07-28 16:07:15 +0200

    init commit/ ogrodje

---
+++ backend/Fritid/src/main/java/com/example/Fritid/FritidBackendApplication.java
@@ -0,0 +1,14 @@
+package com.example.Fritid;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FritidBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FritidBackendApplication.class, args);
+	}
+
+}
---
+++ backend/Fritid/src/test/java/com/example/Fritid/FritidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Fritid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FritidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
